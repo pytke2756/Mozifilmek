b00d113ea96eb7b9ea07767fa077d6ec2431f14a
pytke2756 <ibranyilevente@gmail.com>
2021-10-05 14:30:29 +0200

    hozzaAd(), inputok

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ class Film{
 }
 
 let filmAdatok = [];
+
+function hozzaAd() {
+    let nev = document.getElementById('nev').value;
+    let rendezo = document.getElementById('rendezo').value;
+    let kiadasiEv = document.getElementById('kiadasi_ev').value;
+
+    let obj = new Film(nev, rendezo, kiadasiEv);
+    filmAdatok.push(obj);
+
+    document.getElementById('nev').value = "";
+    document.getElementById('rendezo').value = "";
+    document.getElementById('kiadasi_ev').value = "";
+}
+
+document.getElementById('hozzad_ad').addEventListener('click', hozzaAd);
+console.log(1);
