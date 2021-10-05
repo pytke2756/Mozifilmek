496007c8b89487af65978f7c450afeb11d4c4d17
pytke2756 <ibranyilevente@gmail.com>
2021-10-05 14:05:15 +0200

    Film osztály, filmAdatok tömb

---
+++ script.js
@@ -0,0 +1,10 @@
+class Film{
+
+    constructor(nev, rendezo, kiadasiEv) {
+        this.nev = nev;
+        this.rendezo = rendezo;
+        this.kiadasiEv = kiadasiEv;
+    }
+}
+
+let filmAdatok = [];
