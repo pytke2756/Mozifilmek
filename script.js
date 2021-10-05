e3813aaa95c2bddae59bf44048dc61a0a0407a2d
pytke2756 <ibranyilevente@gmail.com>
2021-10-05 15:00:07 +0200

    listazas(), listazas gomb

--- script.js
+++ script.js
@@ -16,11 +16,26 @@ function hozzaAd() {
 
     let obj = new Film(nev, rendezo, kiadasiEv);
     filmAdatok.push(obj);
+    //console.log(filmAdatok);
+
 
     document.getElementById('nev').value = "";
     document.getElementById('rendezo').value = "";
     document.getElementById('kiadasi_ev').value = "";
 }
 
+function listazas() {
+    let filmAdatokHossz = filmAdatok.length;
+    let adatokString = "";
+    for (let i = 0; i < filmAdatokHossz; i++) {
+              adatokString += "<tr>";
+              adatokString += "<td>" + filmAdatok[i].nev + "</td>";
+              adatokString += "<td>" + filmAdatok[i].rendezo + "</td>";
+              adatokString += "<td>" + filmAdatok[i].kiadasiEv + "</td>";
+              adatokString += "</tr>";
+    }
+    document.getElementById("adatok").innerHTML = adatokString;
+}
+
 document.getElementById('hozzad_ad').addEventListener('click', hozzaAd);
-console.log(1);
+document.getElementById('listazas').addEventListener('click', listazas);
